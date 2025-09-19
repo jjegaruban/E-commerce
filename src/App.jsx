f4a986961e86fc5cf671eb5e293fafd8e36ebda8
jjegaruban <gouthamruban@gmail.com>
2025-09-19 14:27:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  createRoutesFromElements,
+  Route,
+  ScrollRestoration,
+} from "react-router-dom";
+import Footer from "./components/home/Footer/Footer";
+import FooterBottom from "./components/home/Footer/FooterBottom";
+import Header from "./components/home/Header/Header";
+import HeaderBottom from "./components/home/Header/HeaderBottom";
+import SpecialCase from "./components/SpecialCase/SpecialCase";
+import About from "./pages/About/About";
+import SignIn from "./pages/Account/SignIn";
+import SignUp from "./pages/Account/SignUp";
+import Cart from "./pages/Cart/Cart";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/Home/Home";
+import Journal from "./pages/Journal/Journal";
+import Offer from "./pages/Offer/Offer";
+import Payment from "./pages/payment/Payment";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Shop from "./pages/Shop/Shop";
+
+const Layout = () => {
+  return (
+    <div>
+      {/* Background elements */}
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white">
+          <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
+        </div>
+      </div>
+      <Header />
+      <HeaderBottom />
+      <SpecialCase />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
+      <FooterBottom />
+    </div>
+  );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Layout />}>
+        {/* Header Navlink Start */}
+        <Route index element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/journal" element={<Journal />}></Route>
+        {/* Header Navlink End */}
+        <Route path="/offer" element={<Offer />}></Route>
+        <Route path="/product/:_id" element={<ProductDetails />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/paymentgateway" element={<Payment />}></Route>
+      </Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/signin" element={<SignIn />}></Route>
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    <div className="font-bodyFont">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
